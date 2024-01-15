8931aab35de75168cff2b0113ea734eba9c5c817
zimmermr <zimmermanmr@gmail.com>
2024-01-15 16:16:53 -0500

    Added Angular app

--- API/Program.cs
+++ API/Program.cs
@@ -12,13 +12,14 @@ builder.Services.AddDbContext<DataContext>(opt =>
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
+app.UseCors(builder => {
+    builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200");
+});
 
 app.MapControllers();
 
---
+++ client/src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    HttpClientModule
+  ],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  title: string = 'Dating App';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('https://localhost:5001/api/users')
+    .subscribe({
+      next: response => {
+        this.users = response;
+      },
+      error: error => {
+        console.error(error);
+      },
+      complete: () => console.log('Request has completed')
+    });
+  }
+}
